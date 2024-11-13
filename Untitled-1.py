36ee05d49aa9b99aebe48d0f9e0ef21e7ce0e04d
Alex Benny <alexben6g@gmail.com>
2024-11-13 14:16:28 -0800

    authentication

---
+++ Untitled-1.py
@@ -0,0 +1,29 @@
+import os
+import pickle
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']  # or 'calendar' for full access
+
+def authenticate_google_account():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('calendar', 'v3', credentials=creds)
